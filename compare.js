e9907466b6e575e405e0cd32c9db3e8e3f41c29b
Dan Deller <dan.deller@thecontrolgroup.com>
2017-09-07 09:16:09 -0400

    Create compare.js

---
+++ compare.js
@@ -0,0 +1,79 @@
+$(function() {
+
+  // GET SLIDER LIST PRICES
+  //
+  var productHold = [],
+      items       = [];
+
+  var urls = [
+    '/vitapak-brand-store/mens-health-wellness/?format=ajax',
+    '/vitapak-brand-store/mens-engery-metabolism/?format=ajax',
+    '/vitapak-brand-store/mens-spots-performance/?format=ajax',
+    '/vitapak-brand-store/womens-health-wellness/?format=ajax',
+    '/vitapak-brand-store/womens-engery-metabolism/?format=ajax',
+    '/vitapak-brand-store/womens-spots-performance/?format=ajax'
+  ];
+
+  $.when.apply($, urls.map(function(url) {
+    return $.ajax(url);
+  })).done(function() {
+
+    var products = '',
+        tiles    = [];
+
+    for (var i = 0; i < arguments.length; i++) {
+      products = arguments[i][0];
+      tiles.push($(products).find('.product-tile'));
+    };
+
+    for (var i = 0; i < tiles.length; i++) {
+      for (var j = 0; j < tiles[i].length; j++) {
+        var result  = $(tiles[i][j]),
+            name    = $(result).find('.product-name a').attr('href'),
+            nameCut = name.split('/'),
+            nameId  = nameCut[2].split('.'),
+            catId   = nameId[0],
+            price   = result.find('.product-pricing span').text().trim();
+
+        var itemBlock = {
+          id: catId,
+          price: price
+        };
+
+        items.push(itemBlock);
+
+      } // end for tiles[i]
+    } // end for tiles
+
+    compareItems();
+
+  }); // end .done()
+
+
+  // GET PAGE ITEMS ID'S
+  //
+  var pageItems = $('.vitapak-product-div');
+
+  for (var i = 0; i < pageItems.length; i++) {
+    var productInfo = $(pageItems[i]).find('a').attr('href'),
+        productCut  = productInfo.split('/'),
+        product     = productCut[4].split('.html'),
+        productId   = product[0];
+
+    productHold.push(productId);
+  };
+
+
+  // COMPARE AJAX CALL(ITEM) ARRAY & PAGEHOLD ARRAY AND SET PRICES
+  // CALLED AFTER .DONE() IS COMPLETE
+  //
+  function compareItems() {
+    for (var i = 0; i < items.length; i++) {
+      for (var j = 0; j < productHold.length; j++) {
+        if (items[i].id == productHold[j]) {
+          $(pageItems[i]).find('h5 em').text(items[i].price);
+        };
+      };
+    };
+  };
+});
